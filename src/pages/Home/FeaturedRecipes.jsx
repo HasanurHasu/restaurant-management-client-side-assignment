993a94ea5a1a63f2bca017d2da39c6a2662e9f51
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-11-06 22:09:45 +0600

    update Fetured Recipes

--- src/pages/Home/FeaturedRecipes.jsx
+++ src/pages/Home/FeaturedRecipes.jsx
@@ -12,13 +12,15 @@ const FeaturedRecipes = () => {
             .then(data => setTopFoods(data))
     }, [])
 
+    const featuredFood = topFoods.slice(0, 3);
+
     return (
         <div className="mt-12 max-w-6xl mx-auto">
             <h1 className="text-4xl font-bold text-center mb-3">Featured Recipes</h1>
             <p className="text-center w-1/2 mx-auto mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non cumque assumenda, ullam illo consectetur quam voluptate dolores facere aut est!</p>
             <div className="grid grid-cols-3 gap-10">
                 {
-                    topFoods.map(topFood => <FeaturedRecipesCard key={topFood.id} topFood={topFood}></FeaturedRecipesCard>)
+                    featuredFood.map(topFood => <FeaturedRecipesCard key={topFood.id} topFood={topFood}></FeaturedRecipesCard>)
                 }
             </div>
             
