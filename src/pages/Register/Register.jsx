c93464f86e3677bf878c7fd1222b7298efd44bb6
Md. Hasanur Rahman <hasanurhasu121@gmail.com>
2023-11-09 12:03:54 +0600

    update Auth Context

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,55 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
-
+import { AuthContext } from "../../provider/AuthProvider";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { updateProfile } from "firebase/auth";
 
 const Register = () => {
+
+    const {createUser} = useContext(AuthContext);
+    
+    const hanldeRegister= e => {
+        e.preventDefault();
+        const from = e.target;
+        const name = from.name.value;
+        const email = from.email.value;
+        const photoUrl = from.photoUrl.value;
+        const password = from.password.value;
+        const confirmPassword = from.confirmPassword.value;
+
+        if (password.length < 6) {
+            toast.error('Password length under 6')
+            return;
+        }
+        if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+            toast.error('must be capital letter and special  character')
+            return;
+        }
+
+        if (!(password === confirmPassword)){
+            toast.error('Password do not match.')
+            return;
+        }
+
+        createUser(email, password)
+        .then(result => {
+            console.log(result.user);
+            toast.success('User Registration Successfully');
+            updateProfile(result.user, {
+                displayName: name,
+                photoURL: photoUrl
+            })
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
     return (
         <div className="w-1/3 mx-auto my-16">
             <div className="w-full border-2 rounded-md py-5 px-10">
-                <form className="flex flex-col text-lg space-y-3 w-full">
+                <form onSubmit={hanldeRegister} className="flex flex-col text-lg space-y-3 w-full">
                     <h1 className="text-4xl font-bold my-5">Create an account</h1>
                     <div className="form-control">
                         <h2>Name</h2>
@@ -17,7 +61,7 @@ const Register = () => {
                     </div>
                     <div>
                         <h2>Photo URL</h2>
-                        <input type="text" name="text" placeholder="Photo URL" className="border-b-2 w-full input-md pl-0 text-base" />
+                        <input type="text" name="photoUrl" placeholder="Photo URL" className="border-b-2 w-full input-md pl-0 text-base" />
                     </div>
                     <div>
                         <h2>Password</h2>
@@ -25,7 +69,7 @@ const Register = () => {
                     </div>
                     <div>
                         <h2>Confirm Password</h2>
-                        <input type="password" name="password" placeholder="Confirm Password" className="border-b-2 w-full input-md pl-0 text-base" />
+                        <input type="password" name="confirmPassword" placeholder="Confirm Password" className="border-b-2 w-full input-md pl-0 text-base" />
                     </div>
                     <div>
                         <input type="submit" value="Register" className="text-center bg-orange-600 w-full rounded-md mt-2 py-3 text-white font-semibold" />
@@ -48,6 +92,18 @@ const Register = () => {
                 </div>
                 </button>
             </div>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </div>
     );
 };
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,15 +1,50 @@
 import PropTypes from 'prop-types';
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import app from '../firebase/firebase.config';
 
-export const AuthContext = createContext()
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null)
-    const [isLoading, setIsLoading] = useState(true)
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            
+            setLoading(false);
+        });
+        return () => {
+            unSubscribe();
+        }
+    }, [])
 
     const authInfo = {
         user,
-        isLoading
+        loading,
+        createUser,
+        signIn,
+        logOut
     }
+
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
@@ -18,7 +53,7 @@ const AuthProvider = ({ children }) => {
 };
 
 AuthProvider.propTypes = {
-    children : PropTypes.node
-}
+    children: PropTypes.object
+};
 
 export default AuthProvider;
